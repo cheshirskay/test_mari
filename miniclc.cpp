19cbd5614a200e56149ad5543ff94ceb4c3a4c1b
Marina <marysy13@mail.ru>
2020-07-30 14:35:47 +0300

    first commit

---
+++ miniclc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int val1, val2, result;
+    std::cout << "enter the first number" << std::endl;
+    std::cin >> val1;
+    std::cout << "enter the second number" << std::endl;
+    std::cin >> val2;
+    std::cout << "The sum is " << result <<std::endl;
+}
