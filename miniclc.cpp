57cca3921529b27fe699b0d3e14d8dbbff6ac86c
Marina <marysy13@mail.ru>
2020-07-30 15:39:53 +0300

    Second commit

--- miniclc.cpp
+++ miniclc.cpp
@@ -3,9 +3,11 @@
 int main()
 {
     int val1, val2, result;
+    
     std::cout << "enter the first number" << std::endl;
     std::cin >> val1;
     std::cout << "enter the second number" << std::endl;
     std::cin >> val2;
-    std::cout << "The sum is " << result <<std::endl;
+    result = val1 + val2;
+    std::cout << "The sum is "<< result << std::endl;
 }
---
+++ test.cpp
@@ -0,0 +1,2 @@
+result = val1 - val2;
+    std::cout << "The res is "<< result << std::endl;
