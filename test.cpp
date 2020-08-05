332a4b1d766d43bdceda657422bfdf88f3143703
Marina <marysy13@mail.ru>
2020-08-05 20:36:29 +0300

    test_sec

--- test.cpp
+++ test.cpp
@@ -1,2 +1,6 @@
 result = val1 - val2;
     std::cout << "The res is "<< result << std::endl;
+result = val1 * val2;
+    std::cout << "The mult is "<< result << std::endl;
+result = val1 * val2 * val2;
+    std::cout << "The mult is "<< result << std::endl;
